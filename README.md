28988f869af17bb09637fbba1388d1138aa9d242
Jan Müller <jan.mueller@catdev.io>
2021-01-10 16:02:03 +0200

    Inits the role

---
+++ README.md
@@ -0,0 +1,12 @@
+# apt
+
+Configure APT repositories and settings.
+
+## License
+
+This project is licensed under the Apache 2.0 License - see the [LICENSE](LICENSE) file for details.
+
+## Maintainers
+
+[catdev admin](https://github.com/orgs/catdev-admin/people)
+
---
+++ molecule/default/tests/test_default.py
@@ -0,0 +1,19 @@
+import os
+
+import testinfra.utils.ansible_runner
+
+import warnings
+warnings.filterwarnings("ignore", category=DeprecationWarning)
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+
+
+def test_apt_update(host):
+    run = host.run("apt update")
+    assert run.rc == 0
+
+
+def test_apt_unattended_upgrade(host):
+    run = host.run("unattended-upgrade --dry-run")
+    assert run.rc == 0
